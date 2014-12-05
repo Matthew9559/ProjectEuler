7a071fb78c8f27f55103064664e386c7e1626922
Matt Sonnhalter <matt.sonnhalter@gmail.com>
2014-12-04 23:31:03 -0600

    Template to Display Problem Results and Problem 001 Solved

--- ProjectEuler/ProjectEuler/Program.cs
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProjectEuler.Helpers;
+using ProjectEuler.Interfaces;
+using ProjectEuler.Problems;
 
 namespace ProjectEuler
 {
@@ -10,7 +14,20 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Testing Git Commit");
+            var tests = new List<IRunEuler>();
+
+            if (ConfigurationManager.AppSettings["Problem001"].ParseBoolValue())
+                tests.Add(new Problem001() as IRunEuler);
+
+            //Check for Problems to run and display each's answer
+            if (tests != null && tests.Count > 0)
+            {
+                foreach (var test in tests)
+                {
+                    test.Execute();
+                }
+            }
+
             Console.ReadLine();
         }
     }
